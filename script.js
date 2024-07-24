ccf89d438b21f09b8b4bc66df712635a2f769e4d
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2024-07-23 20:12:08 -0700

    link all buttons to script.js
    change the function mathOperation to accept operators as symbols

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+let numbers = document.querySelectorAll('.number');
+let operators = document.querySelectorAll('.operator');
+let CDelete = document.querySelector('#C');
+let DDelete = document.querySelector('#D');
+let period = document.querySelector('#period');
+let equals = document.querySelector('#equals');
 let firstNum;
 let operator;
 let secondNum;
 
 function mathOperations(operator, firstNum, secondNum) {
     switch(operator) {
-        case 'add':
+        case '+':
             return firstNum + secondNum;
             break;
-        case 'subtract':
+        case '-':
             return firstNum - secondNum;
             break;
-        case 'multiply':
+        case '*':
             return firstNum * secondNum;
             break;
-        case 'divide':
+        case '/':
             return firstNum / secondNum;
             break;
     };
