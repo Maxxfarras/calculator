7a1cad7d718e10a8815b11e21dab855e3d1e13da
Maxxfarras <153546976+Maxxfarras@users.noreply.github.com>
2024-07-26 14:26:16 -0700

    format all repo
    ready to merge and push

--- script.js
+++ script.js
@@ -1,95 +1,95 @@
-let numbers = document.querySelectorAll('.number');
-let operators = document.querySelectorAll('.operator');
-let CDelete = document.querySelector('#C');
-let DDelete = document.querySelector('#D');
-let period = document.querySelector('#period');
-let equals = document.querySelector('#equals');
-let topDisplay = document.querySelector('#top-display');
-let bottomDisplay = document.querySelector('#bottom-display');
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+let CDelete = document.querySelector("#C");
+let DDelete = document.querySelector("#D");
+let period = document.querySelector("#period");
+let equals = document.querySelector("#equals");
+let topDisplay = document.querySelector("#top-display");
+let bottomDisplay = document.querySelector("#bottom-display");
 let firstNum = [];
 let joinedNum = 0;
 let operator;
 let wholeOperation = [];
 
 function mathOperations(operator, operandOne, operandTwo) {
-    switch(operator) {
-        case '+':
-            return operandOne + operandTwo;
-        case '-':
-            return operandOne - operandTwo;
-        case 'x':
-            return operandOne * operandTwo;
-        case '÷':
-            return operandOne / operandTwo;
-    };
-};
+  switch (operator) {
+    case "+":
+      return operandOne + operandTwo;
+    case "-":
+      return operandOne - operandTwo;
+    case "x":
+      return operandOne * operandTwo;
+    case "÷":
+      return operandOne / operandTwo;
+  }
+}
 
 function formatNumber(num) {
-    if(Number.isInteger(num)) {
-        return num.toString();
-    } else {
-        return num.toFixed(3);
-    };
-};
+  if (Number.isInteger(num)) {
+    return num.toString();
+  } else {
+    return num.toFixed(3);
+  }
+}
 
 function clearVariables() {
-    [wholeOperation, firstNum].forEach(array => array.length = 0);
-    delete operator;
-    joinedNum = 0;
-    equals.removeEventListener('click', equalFunction);
-};
+  [wholeOperation, firstNum].forEach((array) => (array.length = 0));
+  delete operator;
+  joinedNum = 0;
+  equals.removeEventListener("click", equalFunction);
+}
 
 function equalFunction() {
-    wholeOperation[2] = joinedNum;
-    topDisplay.textContent = `${wholeOperation[0]} ${wholeOperation[1]} ${wholeOperation[2]} =`; //display wholeOperation
-    let selectedOperator = wholeOperation[1];
-    let operandOne = Number(wholeOperation[0]); //convert operands into integers
-    let operandTwo = Number(wholeOperation[2]); 
-    let result = mathOperations(selectedOperator, operandOne, operandTwo); 
-    let roundedNum = formatNumber(result); //format number for 3 decimals
-    bottomDisplay.textContent = roundedNum; 
-    clearVariables();
-    if (bottomDisplay.textContent == 'NaN') {
-        bottomDisplay.textContent = 'ERROR';
-        joinedNum = 0;
-    } else {
-        joinedNum = roundedNum;
-    };
-};
+  wholeOperation[2] = joinedNum;
+  topDisplay.textContent = `${wholeOperation[0]} ${wholeOperation[1]} ${wholeOperation[2]} =`; //display wholeOperation
+  let selectedOperator = wholeOperation[1];
+  let operandOne = Number(wholeOperation[0]); //convert operands into integers
+  let operandTwo = Number(wholeOperation[2]);
+  let result = mathOperations(selectedOperator, operandOne, operandTwo);
+  let roundedNum = formatNumber(result); //format number for 3 decimals
+  bottomDisplay.textContent = roundedNum;
+  clearVariables();
+  if (bottomDisplay.textContent == "NaN") {
+    bottomDisplay.textContent = "ERROR";
+    joinedNum = 0;
+  } else {
+    joinedNum = roundedNum;
+  }
+}
 
 //click event listeners for numbers and period
-[period, ...numbers].forEach(number => {
-    let selectedNumber = number.getAttribute('number');
-    number.addEventListener('click', () => {
-        firstNum.push(selectedNumber); //make an array with all numbers
-        joinedNum = firstNum.join('');
-        bottomDisplay.textContent = joinedNum; //join them into a single string
-        if (wholeOperation.length == 2) {
-            equals.addEventListener('click', equalFunction);
-        };
-    });
+[period, ...numbers].forEach((number) => {
+  let selectedNumber = number.getAttribute("number");
+  number.addEventListener("click", () => {
+    firstNum.push(selectedNumber); //make an array with all numbers
+    joinedNum = firstNum.join("");
+    bottomDisplay.textContent = joinedNum; //join them into a single string
+    if (wholeOperation.length == 2) {
+      equals.addEventListener("click", equalFunction);
+    }
+  });
 });
 
 //click event listeners for operators
-operators.forEach(operator => {
-    let selectedOperator = operator.getAttribute('display');
-    operator.addEventListener('click', () => {
-        wholeOperation[0] = joinedNum;
-        wholeOperation[1] = selectedOperator
-        topDisplay.textContent = `${wholeOperation[0]} ${wholeOperation[1]}`; //displays firstNum + operator
-        firstNum.length = 0; //reset firstNum
-        bottomDisplay.textContent = ''; //reset bottomDisplay
-    });
+operators.forEach((operator) => {
+  let selectedOperator = operator.getAttribute("display");
+  operator.addEventListener("click", () => {
+    wholeOperation[0] = joinedNum;
+    wholeOperation[1] = selectedOperator;
+    topDisplay.textContent = `${wholeOperation[0]} ${wholeOperation[1]}`; //displays firstNum + operator
+    firstNum.length = 0; //reset firstNum
+    bottomDisplay.textContent = ""; //reset bottomDisplay
+  });
 });
 
 //resets all variables, array and display to none
-CDelete.addEventListener('click', () => {
-    [topDisplay, bottomDisplay].forEach(display => display.innerHTML = '');
-    clearVariables();
+CDelete.addEventListener("click", () => {
+  [topDisplay, bottomDisplay].forEach((display) => (display.innerHTML = ""));
+  clearVariables();
 });
 
 //deletes the last element in the firstNum array
-DDelete.addEventListener('click', () => {
-    firstNum.pop(); 
-    bottomDisplay.textContent = firstNum.join('');
+DDelete.addEventListener("click", () => {
+  firstNum.pop();
+  bottomDisplay.textContent = firstNum.join("");
 });
